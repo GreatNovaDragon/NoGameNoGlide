a808ae31c39333574b82c08daffbd0fb90e8739a
JohannesWeitnauer <johannesweitnauer0204@gmail.com>
2016-06-11 16:26:03 +0200

    Add files via upload
    Eine Vorversion des Hin und her bewegens des levels.

---
+++ test.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () {
+}
+
+function Update () {
+    if(Input.GetKeyDown("left"))
+    {
+        transform.Translate(-10, 0, Time.deltaTime);
+  
+    }
+    if(Input.GetKeyDown("right"))
+    {
+        transform.Translate(10, 0, Time.deltaTime);  
+    }
+}
